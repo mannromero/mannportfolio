56509d46b2be64086d45590d6154edf05e10a8cd
mannromero <manuelbanderada@gmail.com>
2022-09-10 11:29:11 +0100

    first commit

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <a href='https://twitter.com/Manuelparafina'>
+      <BsTwitter 
+       />
+       </a>
+    </div>
+    <div>
+      <a href='https://www.facebook.com/manuelsiako25'>
+      <FaFacebookF />
+      </a>
+    </div>
+    <div>
+      <a href='https://www.instagram.com/mnlromero'>
+      <BsInstagram />
+      </a>
+    </div>
+  </div>
+);
+
+export default SocialMedia;
